521926ad8ca2684c3c745ea4de13e0fbe72b31cd
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2024-05-02 19:24:44 -0500

    Nuevos enrutamientos y objetos utilizados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ClientsComponent } from './clients/clients.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainPageComponent, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, MainPageComponent, NavbarComponent, LoginFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ClientsComponent } from './clients/clients.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component:LoginFormComponent},
+    {path: 'main-page', component: MainPageComponent},
+    {path: 'list-clients', component: ClientsComponent},
+
+    
+    {path: '', redirectTo: '/main-page', pathMatch:'full'},
+    {path: '**', redirectTo: '/main-page', pathMatch:'full'},
+];
