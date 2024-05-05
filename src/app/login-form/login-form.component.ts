8982e46bced4a021ee60e98754adc8661cf878c9
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2024-05-05 15:29:05 -0500

    Conexión con la API

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,34 +1,64 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { LoginFormService } from './login-form.service';
+import { HttpClientModule } from '@angular/common/http';
+import Swal from 'sweetalert2';
+
+
+
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterLink, RouterLinkActive,],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, RouterLinkActive, HttpClientModule],
+  providers: [LoginFormService],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
 
+  constructor(private router: Router, private loginService: LoginFormService){}
+
   get user(){
-    return this.loginForm.get('user') as FormControl
+    return this.loginForm.get('nombreUsuario') as FormControl
   }
 
   get password(){
-    return this.loginForm.get('password') as FormControl
+    return this.loginForm.get('claveCuenta') as FormControl
   }
 
-
   loginForm = new FormGroup ({
-    'user': new FormControl('', [Validators.required]),
-    'password': new FormControl('', [Validators.required, Validators.minLength(4)])
+    'nombreUsuario': new FormControl('', [Validators.required]),
+    'claveCuenta': new FormControl('', [Validators.required, Validators.minLength(4)])
   })
 
 
 
   onSubmit(){
-    console.log(this.loginForm.value)
+    this.loginService.getUser({'nombreUsuario': this.loginForm.value.nombreUsuario, 'claveCuenta': this.loginForm.value.claveCuenta}).subscribe(
+      (response: any) =>{
+        if(response){
+          Swal.fire({
+            title: 'Cargando...',
+            text: 'Bienvenido',
+            timer:2000,
+          }).then(() =>{
+            this.router.navigate(['/client'])
+
+          });
+        }
+      }, (error) =>{
+        console.log(error)
+        Swal.fire({
+          title: "Datos incorrectos",
+          text: "Vuelve a ingresar los datos",
+          icon: "error"
+        })
+        this.loginForm.reset();
+        
+      }
+    )
   }
 }
---
+++ src/app/login-form/login-form.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginFormService {
+
+  constructor(private http: HttpClient) { }
+
+  private urlGetUser= 'http://localhost:8000/login/validarUsuarioClave'
+
+  public getUser(expresion: any) {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this.http.post(this.urlGetUser, expresion, { headers: headers, responseType: 'text' })
+  }
+}
