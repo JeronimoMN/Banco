cb8b263667758517d05d84462c27b2761289f212
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2024-05-05 15:29:49 -0500

    Nuevo enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ClientsComponent } from './clients/clients.component';
+import { ClientComponent } from './client/client.component';
 
 export const routes: Routes = [
     {path: 'login', component:LoginFormComponent},
     {path: 'main-page', component: MainPageComponent},
     {path: 'list-clients', component: ClientsComponent},
+    {path: 'client', component: ClientComponent},
 
     
     {path: '', redirectTo: '/main-page', pathMatch:'full'},
