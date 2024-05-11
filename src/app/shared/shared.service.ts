571198511a99bbac476db396be4fe43b72bbc4e7
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2024-05-10 22:36:15 -0500

    Lista de Usuarios Agregada

--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -10,6 +10,7 @@ export class SharedService {
 
   private urlCreateClient = "http://localhost:8000/login/crearcliente"
   private urlCreateAccount = "http://localhost:8000/login/crearcuenta"
+  private ultGetAllUsers = "http://localhost:8000/login/getuserbanck"
 
   public createClient (expresion: any){
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
@@ -19,5 +20,10 @@ export class SharedService {
   public createAccount(expresion:any){
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
     return this.http.post(this.urlCreateAccount, expresion, { headers: headers, responseType: 'text' })
+  }
+
+
+  public getAllUsers(){
+    return this.http.get<any>(this.ultGetAllUsers)
   }
 }
--- src/app/shared/usuarios/usuarios.component.ts
+++ src/app/shared/usuarios/usuarios.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { SharedService } from '../shared.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive, HttpClientModule],
+  providers: [SharedService],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css'
 })
 export class UsuariosComponent {
+  usuarios: any[] = [];
 
+  constructor (private sharedService: SharedService) {
+
+  }
+
+  getUsers(){
+    this.sharedService.getAllUsers().subscribe(data => {
+      this.usuarios= [];
+      data.forEach((element:any[]) => {
+
+        this.usuarios.push({
+          id_usuario: element[0],
+          nombre_completo: element[1],
+          nombre_user: element[2],
+          tipo_usuario: element[3],
+          estado_usuario: element[4]
+        })
+      });
+      console.log(this.usuarios)
+    })
+  }
+
+  ngOnInit(): void{
+    this.getUsers()
+  }
 }
