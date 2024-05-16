23b309a26ecd8e81a270b6dd11466e87be75bba7
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2024-05-16 17:47:17 -0500

    Componente Seguridad agregada.

--- src/app/shared/seguridad/seguridad.component.ts
+++ src/app/shared/seguridad/seguridad.component.ts
@@ -1,12 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { SharedService } from '../shared.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-seguridad',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink, RouterLinkActive, HttpClientModule],
+  providers: [SharedService], 
   templateUrl: './seguridad.component.html',
   styleUrl: './seguridad.component.css'
 })
 export class SeguridadComponent {
+  logTransacciones: any[] = [];
+  logAccesos: any[] = [];
 
+  constructor(private sharedService: SharedService){}
+
+
+  getLogsTransacciones(){
+    this.sharedService.getAllLogsTransacciones().subscribe(data => {
+      this.logTransacciones= [];
+      data.forEach((element:any) => {
+       this.logTransacciones.push({
+        id: element.id,
+        ...element
+       })
+      });
+    })
+  }
+
+  getLogsAccesos(){
+    this.sharedService.getAllLogsAccesos().subscribe(data => {
+      this.logAccesos= [];
+      data.forEach((element:any) => {
+       this.logAccesos.push({
+        id: element.id,
+        ...element
+       })
+      });
+    })
+  }
+
+  ngOnInit(): void{
+    this.getLogsTransacciones()
+    this.getLogsAccesos()
+  }
 }
--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -12,6 +12,8 @@ export class SharedService {
   private urlCreateAccount = "http://localhost:8000/login/crearcuenta"
   private ultGetAllUsers = "http://localhost:8000/login/getuserbanck"
   private ulrChangeStateAccount = "http://localhost:8000/login/cambiarestado"
+  private urlGetAllLogTrassacciones= "http://localhost:8000/login/getlogstrans"
+  private urlGetAllAccess = "http://localhost:8000/login/getlogsaccess"
 
   public createClient (expresion: any){
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
@@ -30,5 +32,13 @@ export class SharedService {
   public changeStateAccount(expresion:any){
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
     return this.http.post(this.ulrChangeStateAccount, expresion, { headers: headers, responseType: 'text' })
+  }
+
+  public getAllLogsTransacciones(){
+    return this.http.get<any>(this.urlGetAllLogTrassacciones)
+  }
+
+  public getAllLogsAccesos(){
+    return this.http.get<any>(this.urlGetAllAccess)
   }
 }
